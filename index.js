430e5a86c63c5f2d23ab2bb5c60b35c8f95848a7
Rubberekip <michelvisser13@gmail.com>
2025-03-01 22:44:46 +0100

    noah

--- index.js
+++ index.js
@@ -1,54 +1,22 @@
-const fs = require("node:fs");
-const path = require("node:path");
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-let { count } = require("./countdata.json");
+import dotenv from "dotenv";
+dotenv.config();
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+import { Client, GatewayIntentBits } from "discord.js";
 
-client.once('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-
-    // Schedule task to run every day at 9:00 AM
-    cron.schedule('0 9 * * *', async () => {
-        const channel = client.channels.cache.get(process.env.CHANNEL_ID);
-        if (!channel) return console.error('Channel not found!');
-
-        await announceCounter();
-
-    });
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages
+    ],
 });
 
-client.on('messageCreate', async (message) => {
-    if (message.content === '!resetcount') {
-        try {
-            const data = { count: 0 };
-            fs.writeFileSync('./countdata.json', JSON.stringify(data, null, 2));
-            await message.channel.send('Het is weer gebeurd hoor');
-            await announceCounter();
-        } catch (error) {
-            console.error('Error resetting count:', error);
-            await message.channel.send('Failed to reset count.');
-        }
-    }
-    if(message.content === "!increaseTest"){
-        await announceCounter();
-    }
-});
 
-const announceCounter = async () => {
-    try {
-        const data = JSON.parse(fs.readFileSync('./countdata.json', 'utf8'));
-        data.count += 1;
-        fs.writeFileSync('./countdata.json', JSON.stringify(data, null, 2));
-        await channel.send(`@everyone Count is now: ${data.count}`);
-    } catch (error) {
-        console.error('Error reading or writing count file:', error);
-    }
-}
 
-client.once(Events.ClientReady, readyClient => {
-	console.log(`Ready! Logged in as ${readyClient.user.tag}`);
-});
+
+
+
+
 
 client.login(process.env.DISCORD_TOKEN);
-
